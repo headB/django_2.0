a9c43b86d27e0d86834d1973cece677fcf0b66b6
kumanxuan <775121173@qq.com>
2018-04-20 14:31:39 +0800

    说明,上一步的注释了,为了就是说明,和django2.0前面的版本不不是同一个用法,还有前一个,其实就是用新版的path换了旧版的url

--- register/views.py
+++ register/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader,RequestContext
 # Create your views here.
 
 def index(request):
 
-    return HttpResponse("渣渣辉")
+    #使用模板文件
+    #1.加载模板文件
+    #然后返回一个模板对象
+    temp = loader.get_template('register/index.html')
+    #2.定义模板上下文
+    #context = RequestContext(request,{})
+    #3. 模板渲染:生成标准的html内容
+    ##说明,上一步的注释了,为了就是说明,和django2.0前面的版本不不是同一个用法
+    res_html = temp.render({},request)
+    #4. 返回给浏览器
+    return HttpResponse(res_html)
